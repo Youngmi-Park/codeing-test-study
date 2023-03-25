c4b997370a1eabf6ceeb41e597bd22e79974bd70
Youngmi Park <yommi1121@naver.com>
2023-03-25 19:49:58 +0900

    Add baekjoon 15650 N과 M (2)

---
+++ baekjoon/backtracking/15650.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/15650
+# 조합, 순서 상관없이 숫자를 뽑는 것
+n, m = map(int, input().split())
+num = []
+
+def dfs(now):
+  if len(num) == m: # 숫자의 개수가 m개이면
+    print(' '.join(map(str, num))) # 수열을 출력
+    return
+
+  for i in range(now, n+1): 
+    if i not in num: # 리스트안에 i가 없다면
+      num.append(i) # i를 추가해줌
+      dfs(i+1) # 현재 숫자보다 큰 숫자만 탐색(작은 경우를 pruning)
+      num.pop()
+    
+dfs(1)
